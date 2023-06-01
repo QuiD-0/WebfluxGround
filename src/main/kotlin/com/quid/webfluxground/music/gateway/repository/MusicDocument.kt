40abdaf82a02d6d439789c39d9475e3d143f6ff7
QuiD-0 <wodnd101@naver.com>
2023-06-01 14:24:12 +0900

    music document 작성

--- src/main/kotlin/com/quid/webfluxground/music/gateway/repository/MusicDocument.kt
+++ src/main/kotlin/com/quid/webfluxground/music/gateway/repository/MusicDocument.kt
@@ -1,9 +1,38 @@
 package com.quid.webfluxground.music.gateway.repository
 
 import com.quid.webfluxground.music.domain.Music
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
 
-class MusicDocument {
+@Document
+class MusicDocument(
+    @Id
+    private val id: ObjectId,
+    @Indexed private val title: String,
+    private val artist: String,
+    private val album: String,
+    private val genre: String,
+    private val duration: Int,
+) {
     fun toDomain(): Music {
-        return Music()
+        return Music(
+            id = id.toHexString(),
+            title = title,
+            artist = artist,
+            album = album,
+            genre = genre,
+            duration = duration,
+        )
     }
 }
+
+fun document(music: Music) = MusicDocument(
+    id = music.id?.let { ObjectId(it) } ?: ObjectId(),
+    title = music.title,
+    artist = music.artist,
+    album = music.album,
+    genre = music.genre,
+    duration = music.duration,
+)
--- src/main/kotlin/com/quid/webfluxground/music/gateway/repository/MusicRepository.kt
+++ src/main/kotlin/com/quid/webfluxground/music/gateway/repository/MusicRepository.kt
@@ -3,9 +3,11 @@ package com.quid.webfluxground.music.gateway.repository
 import com.quid.webfluxground.music.domain.Music
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 interface MusicRepository {
     fun findAll(): Flux<Music>
+    fun save(music: Music): Mono<Music>
 
     @Repository
     class MusicRepositoryImpl(
@@ -13,6 +15,11 @@ interface MusicRepository {
     ): MusicRepository {
         override fun findAll(): Flux<Music> {
             return musicMongoRepository.findAll()
+                .map { it.toDomain() }
+        }
+
+        override fun save(music: Music): Mono<Music> {
+            return musicMongoRepository.save(document(music))
                 .map { it.toDomain() }
         }
 
