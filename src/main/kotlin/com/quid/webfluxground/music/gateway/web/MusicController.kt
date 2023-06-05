831641154dac267ac3505cdab3528cfccb51b999
QuiD-0 <wodnd101@naver.com>
2023-06-05 16:03:28 +0900

    response status 작성

--- src/main/kotlin/com/quid/webfluxground/music/gateway/web/MusicController.kt
+++ src/main/kotlin/com/quid/webfluxground/music/gateway/web/MusicController.kt
@@ -20,14 +20,17 @@ class MusicController(
 ) {
 
     @GetMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
     fun getMusicList(): Flux<MusicResponse> = musicFind.all().flatMap { Mono.just(toMusicResponse(it)) }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createMusic(@RequestBody request: MusicCreateRequest): Mono<MusicResponse> {
         return musicSave.persist(request.toMusic()).map { toMusicResponse(it) }
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.OK)
     fun updateMusic(@RequestBody request: MusicUpdateRequest): Mono<MusicResponse> {
         return musicUpdate.update(request).map { toMusicResponse(it) }
     }
