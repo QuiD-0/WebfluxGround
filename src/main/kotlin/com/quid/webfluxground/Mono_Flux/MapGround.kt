57180c15449faa7fdcf59d02f03ea42354966805
QuiD-0 <wodnd101@naver.com>
2023-06-05 17:05:14 +0900

    map, flatMap

---
+++ src/main/kotlin/com/quid/webfluxground/Mono_Flux/MapGround.kt
@@ -0,0 +1,31 @@
+package com.quid.webfluxground.Mono_Flux
+
+import reactor.core.publisher.Flux
+import kotlin.random.Random
+
+class MapGround {
+
+    private val randomList: List<Int> = List(10000000) { Random.nextInt(0, 1000) }
+    private val flux = Flux.defer { Flux.fromIterable(randomList) }
+
+    fun map() {
+        val timer = System.currentTimeMillis()
+        flux.map { "$it map" }.subscribe()
+        println("map: ${System.currentTimeMillis() - timer}")
+    }
+
+    fun flatMap() {
+        val timer = System.currentTimeMillis()
+        flux.flatMap { Flux.just("$it flatMap") }.subscribe()
+        println("flatMap: ${System.currentTimeMillis() - timer}")
+    }
+
+}
+
+fun main() {
+    val mapGround = MapGround()
+
+    mapGround.map()
+
+    mapGround.flatMap()
+}
