90cc020f60a363a6aa8e79d71456686ffc3b7b65
quid-0 <wodnd101@naver.com>
2023-06-06 18:38:52 +0900

    post FLux

--- src/main/kotlin/com/quid/webfluxground/Stream/StreamController.kt
+++ src/main/kotlin/com/quid/webfluxground/Stream/StreamController.kt
@@ -1,8 +1,12 @@
 package com.quid.webfluxground.Stream
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.time.Duration
 import java.util.*
 import java.util.stream.Stream
 
@@ -11,12 +15,20 @@ import java.util.stream.Stream
 class StreamController {
 
     @GetMapping("/sse")
-    fun stream(): Flux<MutableMap<String, Int>> {
-        val stream = Stream.iterate(0) { i: Int -> i + 1 }
-
-        return Flux.fromStream(stream)
+    fun stream(): Flux<MutableMap<String, Int>> =
+        Stream.iterate(0) { e: Int -> e + 1 }.let {
+            Flux.fromStream(it)
                 .map { i: Int -> Collections.singletonMap("value", i) }
-                .delayElements(java.time.Duration.ofSeconds(1))
-    }
+                .delayElements(Duration.ofSeconds(1))
+        }
+
+    @PostMapping("/postMono")
+    fun echo(@RequestBody body: Mono<String>): Mono<String> =
+        body.map { it.uppercase(Locale.getDefault()) }
+
+    @PostMapping("/postFlux")
+    fun echo(@RequestBody body: Flux<Map<String,Int>>) : Flux<MutableMap<String,Int>> =
+        body.mapNotNull { it["value"]?.times(2) }
+                .map { Collections.singletonMap("double", it) }
 
 }
