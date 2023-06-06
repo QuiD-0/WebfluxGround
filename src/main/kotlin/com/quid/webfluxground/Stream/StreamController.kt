bdcf0c80283e2764c6296fc0d178ca9c1dbaef05
quid-0 <wodnd101@naver.com>
2023-06-06 18:54:44 +0900

    curl 추가

--- src/main/kotlin/com/quid/webfluxground/Stream/StreamController.kt
+++ src/main/kotlin/com/quid/webfluxground/Stream/StreamController.kt
@@ -14,6 +14,9 @@ import java.util.stream.Stream
 @RestController("/stream")
 class StreamController {
 
+    /**
+     * curl -i localhost:8080/sse -H 'Accept: text/event-stream'
+     */
     @GetMapping("/sse")
     fun stream(): Flux<MutableMap<String, Int>> =
         Stream.iterate(0) { e: Int -> e + 1 }.let {
@@ -22,10 +25,20 @@ class StreamController {
                 .delayElements(Duration.ofSeconds(1))
         }
 
+    /**
+     * curl -i localhost:8080/postMono -H 'Content-Type: application/json' -d quid
+     */
     @PostMapping("/postMono")
     fun echo(@RequestBody body: Mono<String>): Mono<String> =
         body.map { it.uppercase(Locale.getDefault()) }
 
+    /**
+     * curl -i localhost:8080/postFlux
+     * -d '{"value":1}{"value":2}{"value":3}'
+     * -H 'Content-Type: application/stream+json'
+     * -H 'Accept: text/event-stream'
+     *
+     * */
     @PostMapping("/postFlux")
     fun echo(@RequestBody body: Flux<Map<String,Int>>) : Flux<MutableMap<String,Int>> =
         body.mapNotNull { it["value"]?.times(2) }
