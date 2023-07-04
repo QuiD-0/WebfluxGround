04ffe966e9e17196fbcf6809d0fff8016ddcb1a7
QuiD-0 <wodnd101@naver.com>
2023-07-04 16:52:22 +0900

    file upload

--- src/main/kotlin/com/quid/webfluxground/file/gateway/api/FileController.kt
+++ src/main/kotlin/com/quid/webfluxground/file/gateway/api/FileController.kt
@@ -4,11 +4,11 @@ import org.springframework.core.io.buffer.DefaultDataBufferFactory
 import org.springframework.http.ContentDisposition
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -18,11 +18,6 @@ import java.io.File
 @RestController
 @RequestMapping("/api/file")
 class FileController {
-
-    @PostMapping
-    fun upload(file: MultipartFile) {
-        println(file)
-    }
 
     @GetMapping
     fun download(exchange: ServerWebExchange): Mono<Void> {
@@ -35,5 +30,17 @@ class FileController {
                 statusCode = HttpStatus.OK
             }
         }.writeWith(Flux.just(dataBuffer))
+    }
+
+    @PostMapping
+    fun upload(exchange: ServerWebExchange): Mono<Void> {
+        val filePartMono = exchange.multipartData
+            .mapNotNull { it["file"]?.get(0) }
+            .cast(FilePart::class.java)
+        return filePartMono.flatMap { filePart ->
+            val destinationFile = File("C:\\Users\\quid\\Desktop\\upload\\${filePart.filename()}")
+            filePart.transferTo(destinationFile)
+                .then(Mono.empty())
+        }
     }
 }
