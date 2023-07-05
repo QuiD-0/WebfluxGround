d3834a0fb6d63a3ea9d9d3152e5758c2fc2cb700
QuiD-0 <wodnd101@naver.com>
2023-07-05 17:14:46 +0900

    refactoring

--- src/main/kotlin/com/quid/webfluxground/stock/gateway/repository/StockRepository.kt
+++ src/main/kotlin/com/quid/webfluxground/stock/gateway/repository/StockRepository.kt
@@ -1,12 +1,28 @@
 package com.quid.webfluxground.stock.gateway.repository
 
 import com.quid.webfluxground.stock.domain.Stock
+import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.math.BigDecimal
 
 interface StockRepository {
 
     fun findByCode(code: Mono<String>): Mono<Stock>
     fun save(stock: Stock): Stock
     fun findAll(): Flux<Stock>
+
+    @Repository
+    class MapStockRepository : StockRepository {
+        private val stocks: MutableMap<String, Stock> = mutableMapOf(
+            "AAPL" to Stock("Apple", BigDecimal(100.0), "USD", "AAPL"),
+            "GOOG" to Stock("Google", BigDecimal(200.0), "USD", "GOOG"),
+            "AMZN" to Stock("Amazon", BigDecimal(300.0), "USD", "AMZN"),
+        )
+
+        override fun findByCode(code: Mono<String>): Mono<Stock> = code.map { stocks[it]?: throw IllegalArgumentException("Stock not found") }
+        override fun save(stock: Stock): Stock = stock.also { stocks[it.code] = it }
+        override fun findAll(): Flux<Stock> { return Flux.fromIterable(stocks.values) }
+
+    }
 }
